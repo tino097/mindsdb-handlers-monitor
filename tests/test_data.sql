4c7f316215b9bd73a04cb433b3e86f6a733083bb
tino097 <konstantin.sivakov@gmail.com>
2025-08-21 16:28:11 +0200

    Fix install command

--- tests/test_data.sql
+++ tests/test_data.sql
@@ -1,6 +1,17 @@
--- Test data for PostgreSQL monitor
-CREATE TABLE IF NOT EXISTS test_table (
-    id SERIAL PRIMARY KEY,
-    value TEXT
+-- Create the table
+CREATE TABLE test (
+    col_one INT,
+    col_two INT,
+    col_three FLOAT,
+    col_four VARCHAR(20)
 );
-INSERT INTO test_table (value) VALUES ('sample1'), ('sample2'), ('sample3');
+
+-- Insert data into the table
+INSERT INTO test (col_one, col_two, col_three, col_four)
+VALUES (1, -1, 0.1, 'A');
+INSERT INTO test (col_one, col_two, col_three, col_four)
+VALUES (2, -2, 0.2, 'B');
+INSERT INTO test (col_one, col_two, col_three, col_four)
+VALUES (3, -3, 0.3, 'C');
+
+COMMIT;
